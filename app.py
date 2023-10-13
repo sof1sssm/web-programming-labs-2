fe9c6916d8b13332c3627d706612f3d5f460cb78
Софья Андронова <sof1sssm@mail.ru>
2023-10-14 01:59:45 +0700

    Имя студента

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -178,3 +178,7 @@ def cats():
     </body>
 </html>
 '''
+@app.route('/lab2/example')
+def example():
+    name = 'Софья Андронова'
+    return render_template('example.html', name=name)
