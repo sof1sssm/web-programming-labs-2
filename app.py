a3466ff1c05e2a8f2f4a5295c5c490b092717f4e
Софья Андронова <sof1sssm@mail.ru>
2023-10-13 22:44:03 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Андронова Софья Александровна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Андрнова Софья, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
