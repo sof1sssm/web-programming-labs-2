9c13a6405931175b12506a29ff5a17146d0212fa
Софья Андронова <sof1sssm@mail.ru>
2023-11-04 23:13:23 +0700

    Заготовка к 4-ой лабораторной

--- lab1.py
+++ lab1.py
@@ -26,6 +26,7 @@ def menu():
         <li><a href="http://127.0.0.1:5000/lab1" target="_blank">Лабораторная работа 1</a></li>
         <li><a href="http://127.0.0.1:5000/lab2/" target="_blank">Лабораторная работа 2</a></li>
         <li><a href="http://127.0.0.1:5000/lab3/" target="_blank">Лабораторная работа 3</a></li>
+        <li><a href="http://127.0.0.1:5000/lab4/" target="_blank">Лабораторная работа 4</a></li>
         </menu>
         <footer>
         &copy; Андронова Софья Александровна, ФБИ-12, 3 курс, 2023
