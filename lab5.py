c2c9b43fc977eababd5579926362b1ea1ed238d5
Софья Андронова <sof1sssm@mail.ru>
2023-12-08 21:51:07 +0700

    Вывод пользователей

--- lab5.py
+++ lab5.py
@@ -1,8 +1,48 @@
-from flask import Blueprint, render_template, request
-lab4 = Blueprint('lab5', __name__)
+from flask import redirect, render_template, request, Blueprint 
+import psycopg2
+
+lab5 = Blueprint('lab5', __name__)
 
 
-@lab5.route('/lab5/')
-def lab():
-    return render_template('lab5.html')
+@lab5.route("/lab5")
+def main():
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="knowledge_base_for_sofia",
+        user="sofia_knowledge_base", 
+        password="123")
 
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    print(result)
+
+    return "go to console"
+
+@lab5.route("/lab5/users")
+def users():
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="knowledge_base_for_sofia",
+        user="sofia_knowledge_base",
+        password="123"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+
+    result = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    print(result)
+
+    return render_template("users.html", result=result)
