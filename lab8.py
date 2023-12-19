d5c0ba064914c1db9f79ea32804a1117a1f9353d
Софья Андронова <sof1sssm@mail.ru>
2023-12-20 02:46:26 +0700

    Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -29,3 +29,12 @@ def get_course(course_num):
     return courses[course_num]
 
 
+#Удаление курса
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num > len(courses)-1:
+        abort(404, "Course not found")
+    del courses[course_num]
+    return '', 204
+
+
