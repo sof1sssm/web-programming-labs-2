6562280dcf78a36c7b9f32c33220cdeabf7a8aa2
Софья Андронова <sof1sssm@mail.ru>
2023-12-28 01:16:28 +0700

    Обработчик ошибки 404

--- lab9.py
+++ lab9.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template, request
 
-lab9= Blueprint('lab9', name)
+lab9= Blueprint('lab9', __name__)
 
 @lab9.route('/lab9/')
 def main():
         return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return "Нет такой страницы", 404
 
 
     
